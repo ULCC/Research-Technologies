a9dacfd3ce383e41888a8c1c38f2e6c8d98e5e27
Timothy Miles-Board <timothy.miles-board@london.ac.uk>
2015-07-15 22:59:31 +0100

    Load incident data from service now, calculate support time used since start of contract period, grab relevant incidents for report

--- generate.pl
+++ generate.pl
@@ -5,22 +5,33 @@ use DateTime;
 use Text::CSV_XS;
 use strict;
 
-my $month = DateTime->now->subtract( months => 1 )->month;
-my $year = DateTime->now->year;
+my $MONTH = DateTime->now->subtract( months => 1 )->month;
+my $YEAR = DateTime->now->year;
+
+# contract period
+# TODO where does this come from?
+my $CPSTART = DateTime->new( year => 2014, month => 8, day => 1 )->epoch;
 
 # reporting period
-my $dt = DateTime->new( year => $year, month => $month, day => 1 );
-my $START = $dt->epoch;
-my $END = $dt->add( months => 1 )->subtract( seconds => 1 )->epoch;
+my $dt = DateTime->new(  year => $YEAR, month => $MONTH, day => 1 );
+my $RPSTART = $dt->epoch;
+my $RPEND = $dt->add( months => 1 )->subtract( seconds => 1 )->epoch;
 
+# load outage data
 my $OUTAGES;
 my $outage_file = $ARGV[0];
 open( my $outage_fh, "<", $outage_file ) or die "Error opening $outage_file: $!";
-&import_outages( $outage_fh );
+&load_outages( $outage_fh );
 
-print Dumper( $OUTAGES );
+# load incident data
+my $INCIDENTS;
+my $incident_file = $ARGV[1];
+open( my $incident_fh, "<", $incident_file ) or die "Error opening $incident_file: $!";
+&load_incidents( $incident_fh );
 
-sub import_outages
+print Dumper( $INCIDENTS );
+
+sub load_outages
 {
     my( $fh ) = @_;
 
@@ -34,17 +45,54 @@ sub import_outages
         my $end = $start + $row->{"Duration (Seconds)"};
 
         # filter outages which didnt start or end in the reporting period
-        next unless ( $START <= $start && $start <= $END ) || ( $START <= $end && $end <= $END );
+        next unless ( $RPSTART <= $start && $start <= $RPEND ) || ( $RPSTART <= $end && $end <= $RPEND );
 
         # work out duration relative to reporting period
-        $start = $START if $start < $START;
-        $end = $END if $end > $END;
+        $start = $RPSTART if $start < $RPSTART;
+        $end = $RPEND if $end > $RPEND;
         my $duration = $end - $start;
 
         my $service = $row->{Server};
         # running total of outage duration (secs) during this period
         $OUTAGES->{$service}{duration_in_period} += $duration;
         push @{ $OUTAGES->{$service}{summary} }, $row;
+    }
+}
+
+sub load_incidents
+{
+    my( $fh ) = @_;
+
+    my $csv = &_init_csv;
+
+	$csv->column_names( @{ $csv->getline( $fh ) } );
+
+	while( my $row = $csv->getline_hr( $fh ) ) 
+	{
+        # convert DD/MM/YYYY HH:MM to YYYY-MM-DD HH:SS
+        # BUG: When both the month and the date are specified in the date as numbers they are always parsed assuming that the month number comes before the date.
+        $row->{"opened_at"} =~ m|^([0-9]{2})/([0-9]{2})/([0-9]{4})\s([0-9]{2}):([0-9]{2})|;
+        my $opened = str2time( "$3-$2-$1 $4:$5" );
+
+        # filter incidents which didn't get opened in the contract period
+        # TODO does an incident opened in a contract period apply to that contract period?
+        next unless ( $CPSTART <= $opened && $opened <= $RPEND );
+
+        my $company = $row->{company};
+        my $time_worked = $row->{category} eq "Incident" ? 0 : $row->{"time_worked"};
+        $INCIDENTS->{$company}{balance_used} += $time_worked;
+
+        # filter incidents not relevant to reporting period
+        $row->{"closed_at"} =~ m|^([0-9]{2})/([0-9]{2})/([0-9]{4})\s([0-9]{2}):([0-9]{2})|;
+        my $closed = str2time( "$3-$2-$1 $4:$5" );
+        my $state = $row->{state};
+        next unless
+            ( defined $closed && $RPSTART <= $closed && $closed <= $RPEND ) # incident closed in reporting period
+            ||
+            ( $state ne "Closed" ) # incident is not closed
+        ;
+
+        push @{ $INCIDENTS->{$company}{summary} }, $row;
     }
 }
 
